ae250664f4469ff80cf1cc93c687e7739a908fe8
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-04-18 19:28:01 +0200

    Add Spring route for React direct access
    * Add an HTTP endpoint in Spring to handle the /back-office endpoint.
    * Add integration tests for the new endpoint.
    * Update readme.

---
+++ src/main/java/com/salaboy/conferences/game/web/WebEndpoints.java
@@ -0,0 +1,36 @@
+package com.salaboy.conferences.game.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class WebEndpoints {
+
+    private static final Logger log = LoggerFactory.getLogger(WebEndpoints.class);
+
+    @Bean
+    public RouterFunction<ServerResponse> indexRouter() {
+        return route()
+                .GET("/back-office", request ->
+                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).bodyValue(getIndexPage()))
+                .build();
+    }
+
+    private Object getIndexPage() {
+        var indexPage = new ClassPathResource("/static/index.html");
+        if (!indexPage.exists()) {
+            // This should only happen when running the app locally in development mode.
+            return "The 'index.html' page doesn't exist.";
+        }
+        return indexPage;
+    }
+
+}
---
+++ src/test/java/com/salaboy/conferences/game/web/WebEndpointsTests.java
@@ -0,0 +1,29 @@
+package com.salaboy.conferences.game.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class WebEndpointsTests {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void whenBackOfficeThenReturnIndexPage() {
+        webTestClient
+                .get()
+                .uri("/back-office")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.TEXT_HTML)
+                .expectBody(String.class).value(body ->
+                    assertThat(body).startsWith("<!DOCTYPE html>"));
+    }
+
+}
