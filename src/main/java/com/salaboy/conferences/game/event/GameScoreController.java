bd7337785c645cd31f077e4f90eece1ca47f1c0a
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-05-03 10:43:53 +0200

    Update cloudevents consumer

--- src/main/java/com/salaboy/conferences/game/event/GameScoreController.java
+++ src/main/java/com/salaboy/conferences/game/event/GameScoreController.java
@@ -1,12 +1,10 @@
 package com.salaboy.conferences.game.event;
 
-import io.cloudevents.CloudEvent;
+import com.salaboy.conferences.game.model.GameScore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("gamescores")
@@ -15,8 +13,10 @@ public class GameScoreController {
     private static final Logger log = LoggerFactory.getLogger(GameScoreController.class);
 
     @PostMapping
-    void addEvent(@RequestBody CloudEvent cloudEvent) {
-        log.info("Event received. Id: {}, type: {}.", cloudEvent.getId(), cloudEvent.getType());
+    GameScore addEvent(@RequestHeader HttpHeaders httpHeaders, @RequestBody GameScore gameScore) {
+        log.info("Event received. Id: {}, type: {}.", httpHeaders.get("ce-id"), httpHeaders.get("ce-type"));
+        log.info("GameScore: {}", gameScore);
+        return gameScore;
     }
 
 }
