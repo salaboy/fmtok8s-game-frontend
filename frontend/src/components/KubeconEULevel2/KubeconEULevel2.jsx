7ec617e125cd2db7462fd1631f551ee1fc87aa2e
salaboy <Salaboy@gmail.com>
2022-05-03 09:49:16 +0100

    update config

--- frontend/src/components/KubeconEULevel2/KubeconEULevel2.jsx
+++ frontend/src/components/KubeconEULevel2/KubeconEULevel2.jsx
@@ -1,4 +1,4 @@
-import "./Level1.scss";
+import "./KubeconEULevel2.scss";
 import React, {useState} from "react";
 import cn from 'classnames';
 import axios from "axios";
@@ -6,7 +6,7 @@ import Button from "../../components/Button/Button";
 import {CountdownCircleTimer} from "react-countdown-circle-timer";
 import PacmanLoader from "react-spinners/PacmanLoader";
 
-function Level1({levelNumber, levelName, functionName, state, dispatch}) {
+function KubeconEULevel2({levelNumber, levelName, functionName, state, dispatch}) {
     const [loading, setLoading] = useState(false);
     const [isSent, setIsSent] = useState(false);
 
@@ -88,13 +88,13 @@ function Level1({levelNumber, levelName, functionName, state, dispatch}) {
                     <div className="Question">
                         <div className="Question__Body">
                             <div className="Question__Number">{levelNumber}</div>
-                            Which OSS project do you like the most?
+                            How many kilometers are between Madrid and Valencia?
                         </div>
                         <div className="Answer">
-                        <Button small block clickHandler={e => sendAnswer(true, false, false, false)}><span className="option">A.</span> Knative</Button>
-                        <Button small block clickHandler={e => sendAnswer(false, true, false, false)}><span className="option">B.</span> Tekton</Button>
-                        <Button small block clickHandler={e => sendAnswer(false, false, true, false)}><span className="option">C.</span> Crossplane</Button>
-                        <Button small block clickHandler={e => sendAnswer(false, false, false, true)}><span className="option">D.</span> Other</Button>
+                        <Button small block clickHandler={e => sendAnswer(true, false, false, false)}><span className="option">A.</span> 348mm</Button>
+                        <Button small block clickHandler={e => sendAnswer(false, true, false, false)}><span className="option">B.</span> 359Mi</Button>
+                        <Button small block clickHandler={e => sendAnswer(false, false, true, false)}><span className="option">C.</span> 359km</Button>
+                        <Button small block clickHandler={e => sendAnswer(false, false, false, true)}><span className="option">D.</span> 223km</Button>
                         </div>
 
                     </div>
@@ -107,4 +107,4 @@ function Level1({levelNumber, levelName, functionName, state, dispatch}) {
 
 }
 
-export default Level1;
+export default KubeconEULevel2;
