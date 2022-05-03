45f2468737c8fae34064ebfec4d1e23987f81cde
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-05-03 21:12:11 +0200

    Add event emitter example

--- src/main/java/com/salaboy/conferences/game/event/GameScoreController.java
+++ src/main/java/com/salaboy/conferences/game/event/GameScoreController.java
@@ -4,14 +4,26 @@ import com.salaboy.conferences.game.model.GameScore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("gamescores")
 public class GameScoreController {
 
     private static final Logger log = LoggerFactory.getLogger(GameScoreController.class);
+
+    private final Sinks.Many<GameScore> gameScoreEmitter = Sinks.many().multicast().onBackpressureBuffer();
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<GameScore> getEvents() {
+        return gameScoreEmitter.asFlux();
+    }
 
     @PostMapping
     void addEvent(@RequestHeader HttpHeaders httpHeaders, @RequestBody GameScore gameScore) {
@@ -21,6 +33,8 @@ public class GameScoreController {
 
         log.info("Event received. Id: {}, type: {}.", httpHeaders.get("ce-id"), httpHeaders.get("ce-type"));
         log.info("GameScore: {}", gameScore);
+
+        gameScoreEmitter.emitNext(gameScore, Sinks.EmitFailureHandler.busyLooping(Duration.ofSeconds(5)));
     }
 
 }
